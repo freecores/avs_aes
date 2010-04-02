acfb6c20a4f9f278f84de6bdc330d18b0192286d
ruschi <ruschi@b7cde3f1-3835-486f-b4a1-e36e8a2d2f5b>
2010-04-02 21:41:27 +0000

    major overhaul to apply for the opencores star

--- sw/AEStester.c
+++ sw/AEStester.c
@@ -1,21 +1,45 @@
+/**
+*  \file AEStester.c
+*  \brief test program and example of how to use the software
+*
+*  This file is part of the project	 avs_aes
+*  see: http://opencores.org/project,avs_aes
+* 
+* \section AUTHORS
+* 	   Thomas Ruschival -- ruschi@opencores.org (www.ruschival.de)
+* 
+* \section LICENSE
+*  Copyright (c) 2009, Authors and opencores.org
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without modification,
+*  are permitted provided that the following conditions are met:
+* 	  * Redistributions of source code must retain the above copyright notice,
+* 	  this list of conditions and the following disclaimer.
+* 	  * Redistributions in binary form must reproduce the above copyright notice,
+* 	  this list of conditions and the following disclaimer in the documentation
+* 	  and/or other materials provided with the distribution.
+* 	  * Neither the name of the organization nor the names of its contributors
+* 	  may be used to endorse or promote products derived from this software without
+* 	  specific prior written permission.
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+*  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+*  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+*  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+*  THE POSSIBILITY OF SUCH DAMAGE	
+*/
 #include <stdio.h>
 #include <string.h>
 #include <avs_aes.h>
-#include <avs_aes_driver.h>
-#define SDRAM_ADDR     0x08000000
-
 
 int main(){   
-	volatile unsigned int* aesctrl       = (unsigned int *)AESCTRLWD;
-	volatile unsigned int* resultport    = (unsigned int *)RESULT_ADDR;
-	volatile unsigned int* dataport      = (unsigned int *)DATA_ADDR;
-	volatile unsigned int* keyport  	 = (unsigned int *)KEY_ADDR;        
  	avs_aes_handle context;
-	
- 
-    //init pointers
-    volatile int* wptr = (int*)SDRAM_ADDR;
-    volatile int* rptr = (int*)SDRAM_ADDR;   
 	int i=0;  
   
     unsigned int result[4];
@@ -24,27 +48,7 @@ int main(){
 		0x55555555,0x66666666,0x77777777,0x88888888};  
 	unsigned int Payload[4] = {
 		0xAA555555,0xBB666666,0xCC777777,0xDD888888};  
-	
-	
-	
 
-// Start MEMTEST   
-    printf("Hello from Nios II!\n"); 
-    // Fill the memory 
-    printf("Filling Memory at %p \n",wptr);
-    i=42;
-    while(i< 4){
-        printf("| %d -> %X |\n",i, wptr);  
-        *(wptr++)=Userkey[i++]; 
-    }
-    i=0;
-    printf("\n I: %d Reading Memory from %X \n",i,rptr);
-
-    while(i< 4){
-        printf("MEM: %X > %X \n",rptr,*rptr);
-        rptr++;
-        i++;
-    }
 
 // START AES-Operations
 	printf("AES-Test\n");
@@ -52,6 +56,7 @@ int main(){
  	avs_aes_setKey(&context,&Userkey);
  	avs_aes_setPayload(&context,&Payload);
   	avs_aes_encrypt(&context);
+
     while(avs_aes_isBusy(&context)){
         printf("not ready\n"); 
     } 
--- sw/avs_aes.c
+++ sw/avs_aes.c
@@ -1,6 +1,49 @@
+/**
+*  \file avs_aes.c
+*  \brief AES Avalon IP Core software driver 
+*
+*  This file is part of the project	 avs_aes
+*  see: http://opencores.org/project,avs_aes
+* 
+* \section AUTHORS
+* 	   Thomas Ruschival -- ruschi@opencores.org (www.ruschival.de)
+* 
+* \section LICENSE
+*  Copyright (c) 2009, Authors and opencores.org
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without modification,
+*  are permitted provided that the following conditions are met:
+* 	  * Redistributions of source code must retain the above copyright notice,
+* 	  this list of conditions and the following disclaimer.
+* 	  * Redistributions in binary form must reproduce the above copyright notice,
+* 	  this list of conditions and the following disclaimer in the documentation
+* 	  and/or other materials provided with the distribution.
+* 	  * Neither the name of the organization nor the names of its contributors
+* 	  may be used to endorse or promote products derived from this software without
+* 	  specific prior written permission.
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+*  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+*  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+*  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+*  THE POSSIBILITY OF SUCH DAMAGE	
+*/
+
+
 #include <avs_aes.h>
 #include <string.h>
 
+/**
+ * \brief setup the context to be used later.
+ * initializes the pointers to the correct memory locations
+ * \param context : struct grouping address information 
+ */
 void avs_aes_init(avs_aes_handle* context){
 	context->key	= (unsigned int*) KEY_ADDR;
 	context->payload= (unsigned int*) DATA_ADDR;
@@ -9,6 +52,13 @@ void avs_aes_init(avs_aes_handle* context){
 	*(context->control) = 0x00000000;
 }
 
+
+/**
+ * \brief setup the context to be used later.
+ * initializes the pointers to the correct memory locations
+ * \param context  struct grouping address information 
+ * \param key user key to load
+ */
 void avs_aes_setKey(avs_aes_handle* context, unsigned int* key){
 	int i=0;
 	unsigned int* target_ptr = (unsigned int* )context->key;
@@ -21,6 +71,13 @@ void avs_aes_setKey(avs_aes_handle* context, unsigned int* key){
 	*(context->control) |= 0x00000080;
 }
 
+
+/**
+ * \brief loads payload for processing to the core
+ * basically memcopy...
+ * \param context  struct grouping address information 
+ * \param payload user data to be processed
+ */
 void avs_aes_setPayload(avs_aes_handle* context, unsigned int* payload){
 	int i=0;
 	unsigned int* target_ptr = (unsigned int* )context->payload;
@@ -30,18 +87,41 @@ void avs_aes_setPayload(avs_aes_handle* context, unsigned int* payload){
 }
 
 
+
+/**
+ * \brief set the KEY_VALID flag in the control word
+ * used to signal the completion of writing the key ( \ref avs_aes_setKey )
+ * \param context  struct grouping address information 
+ */
 void avs_aes_setKeyvalid(avs_aes_handle* context){
 	*(context->control) |= 0x00000080;
 }
 
+
+/**
+ * \brief set the ENCRYPT flag in the control word
+ * start encryption of (hopefully) previously loaded payload 
+ * \param context  struct grouping address information 
+ */
 void avs_aes_encrypt(avs_aes_handle* context){
 	*(context->control) |= 0x00000001;
 }
 
+/**
+ * \brief set the DECRYPT flag in the control word
+ * start encryption of (hopefully) previously loaded payload 
+ * \param context  struct grouping address information 
+ */
 void avs_aes_decrypt(avs_aes_handle* context){
 	*(context->control) |= 0x00000002;
 }
 
+/**
+ * \brief checks the COMPLETED flag
+ * can be used for ugly polling the slave if IRQs are not used
+ * \param context  struct grouping address information 
+ * \return 1 if still computing 0 if done.
+ */
 int avs_aes_isBusy(avs_aes_handle* context){
 	unsigned int mycontrol = *(context->control);	
 	return mycontrol & 0x03; 
